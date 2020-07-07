b2104653f402b069aa7834b0715dedd19d5f69cf
pavi9204 <63219371+pavi9204@users.noreply.github.com>
2020-07-07 11:13:34 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Sample-GitHub pages
+# spring-cloud-gateway
+
+# Required to run the following projects
+1. https://github.com/M-pavi/eureka-server
+2. https://github.com/M-pavi/eureka-client-2
+3. https://github.com/M-pavi/eureka-client-1
+
+# To Create jar
+ `mvn clean package -DskipTests=true`
+
+# Run as jar from terminal
+`java -jar target/spring-cloud-gateway-0.0.1-SNAPSHOT.jar`
+
+# Gateway Architecture
+![Gateway Architecture(architecture.png)
---
+++ src/main/java/in/pavi/router/Router.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ */
+package in.pavi.router;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import in.pavi.throttle.AuthorizationKeyResolver;
+import in.pavi.throttle.RemoteAddressKeyResolver;
+
+/**
+ * @author pavi
+ */
+@Component
+public class Router {
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Value("${spring.profiles.active}")
+	private String activeProfile;
+	@Autowired
+	RemoteAddressKeyResolver remoteAddressKeyResolver;
+	@Autowired
+	AuthorizationKeyResolver authorizationKeyResolver;
+
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder) {
+		logger.debug(this.getClass().getSimpleName() + ": " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		final String client_1_lb = "lb://eureka-client-1";
+		final String client_2_lb = "lb://eureka-client-2";
+		final String defaultLb ="lb://go-to-hell";
+//		if (Set.of("DEV").contains(activeProfile)) {
+//			lb = "lb://eureka-client-1";
+//		} else {
+//			lb = "lb://eureka-client-1";
+//		}
+		return routeLocatorBuilder.routes()
+			/*	.route("client2", r -> r.path("/api/oauth/token")
+						.filters(f -> f.filter((exchange, chain) -> {
+							String path = "/error";
+							ServerHttpRequest request = exchange.getRequest().mutate()
+									.path(path)
+									.build();
+							return chain.filter(exchange.mutate().request(request).build());
+						})).uri(lb))*/
+				.route("client_1", r -> r.path("/1/**")
+					.filters(f -> 
+						f.requestRateLimiter()
+								.rateLimiter(RedisRateLimiter.class, c -> c.setBurstCapacity(10).setReplenishRate(4))
+								// .configure(c -> c.setKeyResolver(exchange ->
+								// Mono.just(HttpHeaders.AUTHORIZATION)))
+								.configure(c -> c.setKeyResolver(authorizationKeyResolver)
+										.setKeyResolver(remoteAddressKeyResolver)).retry(3)
+								.secureHeaders()
+								.addResponseHeader("app", "client1")
+								//.addResponseHeader("response-time", LocalDateTime.now().toString())
+								.hystrix(h -> h.setName("gateway Fallback").setFallbackUri("forward:/default-gateway"))
+						) // add response header
+
+						// .route(r -> r.header("X-Request-Id", "\\d+")
+						.uri(client_1_lb))
+				.route("client_2", r -> r.path("/2/**")
+						.filters(f -> 
+							f.requestRateLimiter()
+									.rateLimiter(RedisRateLimiter.class, c -> c.setBurstCapacity(10).setReplenishRate(4))
+									// .configure(c -> c.setKeyResolver(exchange ->
+									// Mono.just(HttpHeaders.AUTHORIZATION)))
+									.configure(c -> c.setKeyResolver(authorizationKeyResolver)
+											.setKeyResolver(remoteAddressKeyResolver)).retry(3)
+									.secureHeaders()
+									.addResponseHeader("app", "client2")
+									//.addResponseHeader("response-time", LocalDateTime.now().toString())
+									.hystrix(h -> h.setName("gateway Fallback").setFallbackUri("forward:/default-gateway"))
+							) // add response header
+
+							// .route(r -> r.header("X-Request-Id", "\\d+")
+							.uri(client_2_lb))
+				.route("default", r -> r.path("/**").filters(f -> f//.rewritePath("/*", "/default-icms")
+						.hystrix(h -> h.setName("gateway Fallback").setFallbackUri("forward:/fallback/default-gateway")))
+						.uri(defaultLb))
+				.build();
+				
+	}
+	
+	@GetMapping("/fallback/default-gateway")
+	public String defaultGateway() {
+		return "Message from default gateway";
+	}
+	
+}
